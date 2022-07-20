e623d628fb2742d067f6069b31cb780e2a142252
puki4416 <puki4416@gmail.com>
2022-07-20 13:10:39 +0900

    2022-07-20

--- 유형/node.js
+++ 유형/node.js
@@ -1,35 +1,32 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim();
-let tempArr = [];
-let result = "";
-let braket = false;
-for (let i = 0; i < input.length; i++) {
-  if (input[i] === "<") {
-    braket = true;
-    tempArr.reverse();
-    result += tempArr.join("");
-    tempArr = ["<"];
-  } else if (input[i] === ">") {
-    braket = false;
-    tempArr.push(">");
-    result += tempArr.join("");
-    tempArr = [];
-  } else if (input[i] === " ") {
-    if (braket) {
-      tempArr.push(" ");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+let dpArr = [[1n]];
+let result = 0n;
+
+for (let i = 1; i < 201; i++) {
+  dpArr[i] = [];
+  for (let j = 0; j < dpArr[i - 1].length + 1; j++) {
+    if (j === 0) {
+      dpArr[i][0] = dpArr[i - 1][0];
+    } else if (j === dpArr[i - 1].length) {
+      dpArr[i][dpArr[i - 1].length] = dpArr[i - 1][dpArr[i - 1].length - 1];
     } else {
-      tempArr.reverse();
-      tempArr.push(" ");
-      result += tempArr.join("");
-      tempArr = [];
+      dpArr[i][j] = dpArr[i - 1][j] + dpArr[i - 1][j - 1];
     }
-  } else {
-    tempArr.push(input[i]);
   }
 }
-if (tempArr.length > 0) {
-  tempArr.reverse();
-  result += tempArr.join("");
+if (input[1] >= input[0]) {
+  for (let i = 0; i < input[0]; i++) {
+    result += (dpArr[input[1]][i + 1] * dpArr[input[0] - 1][i]) % 1000000000n;
+  }
+} else {
+  for (let i = 0; i < input[1]; i++) {
+    result += (dpArr[input[1]][i + 1] * dpArr[input[0] - 1][i]) % 1000000000n;
+  }
 }
-
-console.log(result);
+console.log((result % 1000000000n).toString());
