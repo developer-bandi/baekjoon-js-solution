8f0cc48a29a5c9e9f27ee528279b4f7faee2c386
puki4416 <puki4416@gmail.com>
2022-07-28 12:19:50 +0900

    2022-07-28

--- 유형/node.js
+++ 유형/node.js
@@ -1,38 +1,22 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const lengthArr = input[1].split(" ").map(BigInt);
+const priceArr = input[2].split(" ").map(BigInt);
 
-const numberCardArr = input[1]
-  .split(" ")
-  .map(Number)
-  .sort(function (a, b) {
-    return a - b;
-  });
-const sangenHasArr = input[3].split(" ").map(Number);
-
-function Search() {
-  const resultArr = [];
-  for (let i = 0; i < sangenHasArr.length; i++) {
-    let start = 0;
-    let mid = 0;
-    let end = numberCardArr.length - 1;
-    let result = null;
-    while (start <= end) {
-      mid = Math.floor((start + end) / 2);
-      if (numberCardArr[mid] > sangenHasArr[i]) {
-        end = mid - 1;
-      } else if (numberCardArr[mid] < sangenHasArr[i]) {
-        start = mid + 1;
-      } else {
-        result = mid;
-        break;
-      }
-    }
-    if (result === null) {
-      resultArr[i] = 0;
-    } else {
-      resultArr[i] = 1;
+for (let i = 0; i < priceArr.length; i++) {
+  if (i === 0) {
+    continue;
+  } else {
+    if (priceArr[i] > priceArr[i - 1]) {
+      priceArr[i] = priceArr[i - 1];
     }
   }
-  return resultArr;
 }
-console.log(Search().join(" "));
+
+console.log(
+  lengthArr
+    .reduce((prev, curent, index) => {
+      return prev + curent * priceArr[index];
+    }, 0n)
+    .toString()
+);
