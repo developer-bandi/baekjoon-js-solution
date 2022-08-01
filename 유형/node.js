09882d0f7510cb10bbcb3a6bc5d2c28feb1f39f6
puki4416 <puki4416@gmail.com>
2022-08-01 14:40:10 +0900

    2022-08-01

--- 유형/node.js
+++ 유형/node.js
@@ -1,30 +1,20 @@
 const fs = require("fs");
-const [n, ...input] = fs
+const input = fs
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split(/\s/)
+  .split("")
   .map(Number);
 
-const dpArr = new Array(3 * input.length).fill(0);
-dpArr[1] = input[0];
+const inputSum = input.reduce((prev, current) => {
+  return prev + current;
+}, 0);
 
-for (let i = 1; i < input.length; i++) {
-  if (dpArr[(i - 1) * 3] > dpArr[i * 3 + 2]) {
-    dpArr[i * 3 + 2] = dpArr[(i - 1) * 3];
-  }
-  if (dpArr[(i - 1) * 3 + 1] + input[i] > dpArr[i * 3]) {
-    dpArr[i * 3] = dpArr[(i - 1) * 3 + 1] + input[i];
-  }
-  if (dpArr[(i - 1) * 3 + 1] > dpArr[i * 3 + 2]) {
-    dpArr[i * 3 + 2] = dpArr[(i - 1) * 3 + 1];
-  }
-  if (dpArr[(i - 1) * 3 + 2] + input[i] > dpArr[i * 3 + 1]) {
-    dpArr[i * 3 + 1] = dpArr[(i - 1) * 3 + 2] + input[i];
-  }
-  if (dpArr[(i - 1) * 3 + 2] > dpArr[i * 3 + 2]) {
-    dpArr[i * 3 + 2] = dpArr[(i - 1) * 3 + 2];
-  }
+if (inputSum % 3 === 0 && input.indexOf(0) !== -1) {
+  input.sort(function (a, b) {
+    return b - a;
+  });
+  console.log(input.join(""));
+} else {
+  console.log(-1);
 }
-
-console.log(Math.max(...dpArr));
