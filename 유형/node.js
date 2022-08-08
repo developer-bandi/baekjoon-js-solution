1df806cd0e57f9c557baff0f6edff5bdc08a0a7a
puki4416 <puki4416@gmail.com>
2022-08-08 15:52:44 +0900

    2022-08-08

--- 유형/node.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
-const dpArr = [0n, 1n, 2n];
-for (let i = 3; i <= 1000000; i++) {
-  dpArr[i] = (dpArr[i - 1] + dpArr[i - 2]) % 15746n;
-}
-
-console.log((dpArr[input] % 15746n).toString());
