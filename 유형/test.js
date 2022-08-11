37747469908221d199e7eca40739ad4e83a242ae
puki4416 <puki4416@gmail.com>
2022-08-11 20:18:53 +0900

    2022-08-11

--- 유형/test.js
+++ 유형/test.js
@@ -1,15 +1,47 @@
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [N, k] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+let start = 1;
+let mid = 0;
+let end = N ** 2;
+let result = 0;
+while (start <= end) {
+  mid = Math.floor((start + end) / 2);
 
-let searchStart = 0;
-let count = 0;
-while (true) {
-  if (input[0].indexOf(input[1], searchStart) !== -1) {
-    count++;
-    searchStart = input[0].indexOf(input[1], searchStart) + input[1].length;
-  } else {
+  let count = 0;
+  for (let i = 1; i <= N; i++) {
+    if (count > k) break;
+    if (mid > i * N) {
+      count += N;
+    } else {
+      count += Math.floor(mid / i);
+    }
+  }
+  if (count > k) {
+    result = mid;
+    end = mid - 1;
+  } else if (count === k) {
+    result = mid;
     break;
+  } else {
+    start = mid + 1;
   }
 }
 
-console.log(count);
+for (let i = result; i > 0; i--) {
+  let flag = false;
+  for (let j = 1; j <= N; j++) {
+    if (i % j === 0 && i / j <= N) {
+      flag = true;
+      break;
+    }
+  }
+  if (flag) {
+    console.log(i);
+    break;
+  }
+}
