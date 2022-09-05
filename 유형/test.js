2eb25b67cb4222554e55747b329f91b6fadb631b
puki4416 <puki4416@gmail.com>
2022-09-05 14:24:38 +0900

    2022-09-05

--- 유형/test.js
+++ 유형/test.js
@@ -1,38 +1,28 @@
 const fs = require("fs");
-const [N, ...board] = fs
+const input = fs
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split("\n");
-const [height, width] = N.split(" ").map(Number);
-for (let i = 0; i < board.length; i++) {
-  board[i] = board[i].split(" ").map(Number);
-}
-const dpArr = new Array(height).fill(0).map(() => {
-  return new Array(width).fill(0);
-});
-dpArr[0][0] = board[0][0];
-out: for (let i = 0; i < height; i++) {
-  for (let j = 0; j < width; j++) {
-    if (i === height - 1 && j === width - 1) break out;
-    if (i + 1 < height) {
-      dpArr[i + 1][j] = Math.max(
-        dpArr[i][j] + board[i + 1][j],
-        dpArr[i + 1][j]
-      );
-    }
-    if (j + 1 < width) {
-      dpArr[i][j + 1] = Math.max(
-        dpArr[i][j] + board[i][j + 1],
-        dpArr[i][j + 1]
-      );
-    }
-    if (i + 1 < height && j + 1 < width) {
-      dpArr[i + 1][j + 1] = Math.max(
-        dpArr[i][j] + board[i + 1][j + 1],
-        dpArr[i + 1][j + 1]
-      );
+  .split(/\s/)
+  .map(Number);
+
+const dpArr = [0];
+
+for (let i = 1; i < input.length; i++) {
+  let start = 0;
+  let end = dpArr.length - 1;
+  let mid = 0;
+  let result = null;
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2);
+    if (dpArr[mid] < input[i]) {
+      result = mid;
+      start = mid + 1;
+    } else {
+      end = mid - 1;
     }
   }
+  dpArr[result + 1] = input[i];
 }
-console.log(dpArr[height - 1][width - 1]);
+
+console.log(input.length - dpArr.length);
