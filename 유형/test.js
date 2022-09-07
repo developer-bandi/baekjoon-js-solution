19f8a0c645187bf2baceefcfe8beb260e9250a0f
puki4416 <puki4416@gmail.com>
2022-09-07 15:41:49 +0900

    2022-09-07

--- 유형/test.js
+++ 유형/test.js
@@ -1,26 +1,30 @@
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [H, W] = input[0].split(" ").map(Number);
+const board = new Array(H);
+for (let i = 0; i < board.length; i++) {
+  board[i] = input[i + 1].trim().split("");
+}
+let visit = new Array(26).fill(false);
+let result = 0;
+const dx = [0, 0, 1, -1];
+const dy = [1, -1, 0, 0];
 
-const dpArr = [];
-const findTime = (i) => {
-  const current = input[i].split(" ").map(Number);
-  let time = 0;
-  for (let j = 1; j < current.length - 1; j++) {
-    if (dpArr[current[j]] !== undefined) {
-      time = Math.max(time, dpArr[current[j]]);
-    } else {
-      findTime(current[j]);
-      time = Math.max(time, dpArr[current[j]]);
+function DFS(x, y, cnt) {
+  result = Math.max(result, cnt);
+  for (let i = 0; i < 4; i++) {
+    let nx = x + dx[i];
+    let ny = y + dy[i];
+    if (nx >= 0 && ny >= 0 && nx < R && ny < C) {
+      if (visit[board[nx][ny].charCodeAt() - 65] === false) {
+        visit[board[nx][ny].charCodeAt() - 65] = true;
+        DFS(nx, ny, cnt + 1);
+        visit[board[nx][ny].charCodeAt() - 65] = false;
+      }
     }
   }
-  dpArr[i] = time + current[0];
-};
-
-for (let i = 1; i < input.length; i++) {
-  if (dpArr[i] === undefined) {
-    findTime(i);
-  }
 }
-dpArr.shift();
 
-console.log(dpArr.join("\n"));
+visit[board[0][0].charCodeAt() - 65] = true;
+DFS(0, 0, 1);
+console.log(result);
