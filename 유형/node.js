9cfbc6badbdba691b6ce60a8b08336d3e8c48972
김상두 <puki4416@gmail.com>
2022-10-20 14:28:52 +0900

    2022-10-20

--- 유형/node.js
+++ 유형/node.js
@@ -2,104 +2,38 @@ const input = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split("\n");
-input.shift();
-let queue = [[]];
-const visited = [];
-let result = -1;
-const move = [
-  [0, 1],
-  [0, -1],
-  [1, 0],
-  [-1, 0],
-];
-for (let i = 0; i < input.length; i++) {
-  const row = input[i].split("");
-  for (let j = 0; j < row.length; j++) {
-    if (row[j] === "B") {
-      queue[0][0] = i;
-      queue[0][1] = j;
-      row[j] === ".";
-    }
-    if (row[j] === "R") {
-      queue[0][2] = i;
-      queue[0][3] = j;
-      row[j] === ".";
-    }
-  }
-  input[i] = row;
-}
-
-out: for (let i = 0; i < 10; i++) {
-  const temptQueue = [];
-  for (let j = 0; j < queue.length; j++) {
-    for (let k = 0; k < 4; k++) {
-      const tempt = [];
-      const moveCount = [];
-      let hole = [];
-      for (let h = 1; h < 11; h++) {
-        if (
-          input[queue[j][0] + move[k][0] * h][queue[j][1] + move[k][1] * h] ===
-          "#"
-        ) {
-          tempt.push(queue[j][0] + move[k][0] * (h - 1));
-          tempt.push(queue[j][1] + move[k][1] * (h - 1));
-          moveCount.push(h);
-          break;
-        }
-        if (
-          input[queue[j][0] + move[k][0] * h][queue[j][1] + move[k][1] * h] ===
-          "O"
-        ) {
-          hole[0] = 0;
-          break;
-        }
+  .split("")
+  .map(Number);
+input.unshift(1);
+let count = [1n, 1n];
+if (input[1] === 0) {
+  console.log(0);
+} else {
+  for (let i = 2; i < input.length; i++) {
+    if (input[i - 1] === 1) {
+      if (input[i] === 0) {
+        count.push(count[i - 2]);
+      } else {
+        count.push(count[i - 1] + count[i - 2]);
       }
-      for (let h = 1; h < 11; h++) {
-        if (
-          input[queue[j][2] + move[k][0] * h][queue[j][3] + move[k][1] * h] ===
-          "#"
-        ) {
-          tempt.push(queue[j][2] + move[k][0] * (h - 1));
-          tempt.push(queue[j][3] + move[k][1] * (h - 1));
-          moveCount.push(h);
-          break;
-        }
-        if (
-          input[queue[j][2] + move[k][0] * h][queue[j][3] + move[k][1] * h] ===
-          "O"
-        ) {
-          hole[1] = 0;
-          break;
-        }
+    } else if (input[i - 1] === 2) {
+      if (input[i] === 0) {
+        count.push(count[i - 2]);
+      } else if (input[i] < 7) {
+        count.push(count[i - 1] + count[i - 2]);
+      } else {
+        count.push(count[i - 1]);
       }
-
-      if (hole[0] !== 0 && hole[1] === 0) {
-        result = i + 1;
-        break out;
-      }
-      if (
-        (hole[0] === 0 && hole[1] === 0) ||
-        (hole[0] === 0 && hole[1] !== 0)
-      ) {
-        continue;
-      }
-      if (tempt[0] === tempt[2] && tempt[1] === tempt[3]) {
-        if (moveCount[0] > moveCount[1]) {
-          tempt[0] -= move[k][0];
-          tempt[1] -= move[k][1];
-        } else {
-          tempt[2] -= move[k][0];
-          tempt[3] -= move[k][1];
-        }
-      }
-      const position = tempt.join(" ");
-      if (visited.indexOf(position) === -1) {
-        temptQueue.push(tempt);
-        visited.push(position);
+    } else {
+      if (input[i] === 0) {
+        count = 0;
+        break;
+      } else {
+        count.push(count[i - 1]);
       }
     }
   }
-  queue = temptQueue;
+  console.log(
+    count === 0 ? 0 : (count[count.length - 1] % 1000000n).toString()
+  );
 }
-console.log(result);
