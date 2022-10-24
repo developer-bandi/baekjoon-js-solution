7b4d8c5503fb493b8a360fd816bf431426ce8717
김상두 <puki4416@gmail.com>
2022-10-24 15:59:43 +0900

    2022-10-24

--- 유형/node.js
+++ 유형/node.js
@@ -2,52 +2,26 @@ const input = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split("\n");
+  .split(" ")
+  .map((data, index) => {
+    if (index === 0) return BigInt(data) - 1n;
+    return BigInt(data);
+  });
 
-const T = Number(input[0]);
-const A = input[2].split(" ").map(Number);
-const B = input[4].split(" ").map(Number);
-const saveArr = {};
-let result = 0;
-
-for (let i = 1; i < A.length; i++) {
-  A[i] += A[i - 1];
+const saved = [0n, 1n];
+for (let i = 2n; i < 64n; i = i + 1n) {
+  saved[i] = saved[i - 1n] + (2n ** (i - 1n) * (i - 1n)) / 2n + 2n ** (i - 1n);
+}
+const result = [];
+const sum = (target) => {
+  if (target === 0n) return 0n;
+  if (target === 1n) return 1n;
+  const binary = target.toString(2);
+  const decimal = target % 2n ** BigInt(binary.length - 1);
+  return saved[binary.length - 1] + sum(decimal) + decimal + 1n;
+};
+for (let i = 0; i < input.length; i++) {
+  result[i] = sum(input[i]);
 }
 
-for (let i = 1; i < B.length; i++) {
-  B[i] += B[i - 1];
-}
-
-for (let i = 0; i < A.length; i++) {
-  for (let j = i - 1; j >= -1; j--) {
-    if (j === -1) {
-      if (saveArr[T - A[i]] === undefined) {
-        saveArr[T - A[i]] = 1;
-      } else {
-        saveArr[T - A[i]] += 1;
-      }
-    } else {
-      if (saveArr[T - A[i] + A[j]] === undefined) {
-        saveArr[T - A[i] + A[j]] = 1;
-      } else {
-        saveArr[T - A[i] + A[j]] += 1;
-      }
-    }
-  }
-}
-
-for (let i = 0; i < B.length; i++) {
-  for (let j = i - 1; j >= -1; j--) {
-    if (j === -1) {
-      if (saveArr[B[i]] !== undefined) {
-        result += saveArr[B[i]];
-      }
-    } else {
-      if (saveArr[B[i] - B[j]] !== undefined) {
-        result += saveArr[B[i] - B[j]];
-      }
-    }
-  }
-}
-
-console.log(result);
+console.log((result[1] - result[0]).toString());
