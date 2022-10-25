fa9e93585723482315dfa5ec52328953121ea6fe
김상두 <puki4416@gmail.com>
2022-10-25 14:46:08 +0900

    2022-10-25

--- 유형/node.js
+++ 유형/node.js
@@ -2,26 +2,26 @@ const input = require("fs")
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
-  .split(" ")
-  .map((data, index) => {
-    if (index === 0) return BigInt(data) - 1n;
-    return BigInt(data);
-  });
+  .split("\n");
 
-const saved = [0n, 1n];
-for (let i = 2n; i < 64n; i = i + 1n) {
-  saved[i] = saved[i - 1n] + (2n ** (i - 1n) * (i - 1n)) / 2n + 2n ** (i - 1n);
+const target = Number(input[0]);
+const targetLength = input[0].length;
+const deletedNumber = input[1] === "0" ? [] : input[2].split(" ").map(Number);
+let result = Math.abs(target - 100);
+for (let i = 0; i < 500000; i++) {
+  let plag = [true, true];
+  let plus = target + i;
+  let minus = target - i;
+  while (minus !== 0 && !flag) {
+    if (deletedNumber.indexOf(minus % 10) !== -1) {
+      break;
+    }
+    minus = Math.floor(minus / 10);
+  }
+  while (plus !== 0) {
+    if (deletedNumber.indexOf(plus % 10) !== -1) {
+      break;
+    }
+    plus = Math.floor(plus / 10);
+  }
 }
-const result = [];
-const sum = (target) => {
-  if (target === 0n) return 0n;
-  if (target === 1n) return 1n;
-  const binary = target.toString(2);
-  const decimal = target % 2n ** BigInt(binary.length - 1);
-  return saved[binary.length - 1] + sum(decimal) + decimal + 1n;
-};
-for (let i = 0; i < input.length; i++) {
-  result[i] = sum(input[i]);
-}
-
-console.log((result[1] - result[0]).toString());
